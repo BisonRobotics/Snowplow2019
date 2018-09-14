1070960e821090c6a2018860e0e1fb755f920a5e
JoeyCluett <joesunglassescool@gmail.com>
2018-09-13 19:09:44 -0500

    cautionary push

--- test/sicksensormsgsplit.cpp
+++ test/sicksensormsgsplit.cpp
@@ -9,6 +9,5 @@ int main(int argc, char* argv[]) {
     SickSensor ss("localhost", 13000, false); // dont actually connect to SICK
     ss.scanData("txt/sick-response.txt");
 
-    
-
+    return 0;
 }
