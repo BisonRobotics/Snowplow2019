15bc7699e83960d522884eacdef0c96dbfcd3097
SevenSignBits <joesunglassescool@gmail.com>
2019-01-21 13:26:17 -0600

    Update build.sh

--- nodes/pathplannerdata/build.sh
+++ nodes/pathplannerdata/build.sh
@@ -2,6 +2,6 @@
 
 INC_OPTS="-I./../../../CPJL/lib/cpp/ -I./../../../CPJL/lib/gen/messages/ -I./../../inc/"
 STD_OPTS="-std=c++11 -march=native -O3 -ffast-math -lpthread -Wall -fno-strict-aliasing -lSDL"
-OBJ=" "
+OBJ="./../../lib/misc.o"
 
 g++ -o main.exe main.cpp ${OBJ} ${INC_OPTS} ${STD_OPTS}
