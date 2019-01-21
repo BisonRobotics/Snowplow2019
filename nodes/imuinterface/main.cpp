b06c33fc060111a4f911fcd2573cf26ea13a94c4
Zach O'Brien <zach.m.obrien@gmail.com>
2019-01-21 16:44:34 -0600

    Translating IMU coordinates to robot coordinates

--- nodes/imuinterface/main.cpp
+++ nodes/imuinterface/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdio.h>
+#include <math.h>
 #include <CPJL.hpp>
 #include <cpp/ImuData.h>
 
@@ -7,6 +9,14 @@
 
 #include "main.h"
 
+
+#define CALIBRATION_IMU_TO_ROBO_AZIMUTH     (0)
+#define CALIBRATION_IMU_TO_ROBO_ZENITH      (0)
+#define PI                                  (3.14159265)
+#define DEG_TO_RAD(x)                       (x * (PI/180))
+#define RAD_TO_DEG(x)                       (x * (180/PI))
+
+
 using namespace std;
 using namespace vn::math;
 using namespace vn::sensors;
@@ -14,6 +24,23 @@ using namespace vn::protocol::uart;
 using namespace vn::xplat;
 
 std::string topicname = "vecnavdata";
+
+void imu_vector_to_robo_vector(float &x, float &y, float &z)
+{
+    // Convert to sperical coordinates
+    float r = sqrt((x*x) + (y*y) + (z*z));
+    float azimuth = atanf(z/sqrt(x*x + y*y)); // the skewered egg
+    float zenith = atanf(y/x); // theta
+
+    // Apply transformation with values obtained from calibration
+    azimuth -= CALIBRATION_IMU_TO_ROBO_AZIMUTH;
+    zenith += CALIBRATION_IMU_TO_ROBO_ZENITH;
+
+    // Convert back to cartesian
+    x = r * sin(zenith) * cos(azimuth);
+    y = r * sin(zenith) * sin(azimuth);
+    z = r * cos(zenith);
+}
 
 int main(int argc, char* argv[]) {
 
@@ -35,24 +62,37 @@ int main(int argc, char* argv[]) {
         imu->timestamp = (++timestamp_val);
 
         // grab all data from VecNav
-        auto cd = ex->currentData();
+        auto cd = ez->currentData();
 
         // fill out appropriate message fields if data is 
         // available. if no new data is ready, send the 
         // previous value
+        float tempx, tempy, tempz;
         if(cd.hasYawPitchRoll()) {
             auto ypr = cd.yawPitchRoll();
-            imu->z_orient = ypr.z;
+            tempx = ypr.x;
+            tempy = ypr.y;
+            tempz = ypr.z;
+            imu_vector_to_robo_vector(tempx,tempy,tempz);
+            imu->z_orient = tempz;
         }
         if(cd.hasAcceleration()) {
             auto accel = cd.acceleration();
-            imu->x_acc = accel.x;
-            imu->y_acc = accel.y;
+            tempx = accel.x;
+            tempy = accel.y;
+            tempz = accel.z;
+            imu_vector_to_robo_vector(tempx,tempy,tempz);
+            imu->x_acc = tempx;
+            imu->y_acc = tempy;
         }
         if(cd.hasAnyVelocity()) {
             auto vel = cd.anyVelocity();
-            imu->x_vel = vel.x;
-            imu->y_vel = vel.y;
+            tempx = vel.x;
+            tempy = vel.y;
+            tempz = vel.z;
+            imu_vector_to_robo_vector(tempx,tempy,tempz);
+            imu->x_vel = tempx;
+            imu->y_vel = tempy;
         }
 
         // send data
