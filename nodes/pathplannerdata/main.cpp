76e73398dcfb9b5406c3b5ad4cde1486b6008470
Zach O'Brien <zach.m.obrien@gmail.com>
2019-01-22 19:20:38 -0600

    Moved speed control to path planner

--- nodes/pathplannerdata/main.cpp
+++ nodes/pathplannerdata/main.cpp
@@ -13,6 +13,18 @@
 #include <misc.h>
 #include <unistd.h>
 
+#define ENCODER_TICKS_PER_ROTATION      (2048.0)
+#define DEADZONE                        (1)
+#define WHEEL_CIRCUMFERENCE             (1.39) // meters
+
+
+enum wheels_e
+{
+    LEFT_WHEEL = 0,
+    RIGHT_WHEEL,
+    NUM_OF_WHEELS
+};
+
 using namespace std;
 
 XboxData* xbox_data_rx = NULL;
@@ -22,9 +34,10 @@ PathVector* AutoVector = NULL;
 Encoder* encoder = NULL;
 
 uint64_t last_timestamp = -1;
+float left_setpoint = 0, right_setpoint = 0;
 
-#define ENCODER_TICKS_PER_ROTATION      (2048.0)
-#define WHEEL_CIRCUMFERENCE  25.0 // meters
+mutex setpoint_mutex;
+
 
 float current_x_acc;
 float current_y_acc;
@@ -34,6 +47,22 @@ float current_z_orient;
 float last_z_orient;
 float distanceTraveled_meters;
 
+
+int clamp(int value, const int min, const int max) {
+    int returnVal = value;
+    if(value > max)
+        returnVal = max;
+    else if(value < min)
+        returnVal = min;
+    return returnVal;
+}
+
+double encoderDataToRPM(double encoderTicks, double timeDelta)
+{
+    double rotations = encoderTicks / ENCODER_TICKS_PER_ROTATION; // number of rotations since last poll
+    double rpm = rotations / timeDelta;
+    return rpm;
+}
 
 void xbox_callback(void)
 {
@@ -83,6 +112,67 @@ void encoder_callback(void){
     rotations += encoder->right / ENCODER_TICKS_PER_ROTATION;
     rotations /=2; // get the average rotations of the encoders
     distanceTraveled_meters = rotations * WHEEL_CIRCUMFERENCE;
+
+    static uint64_t last_encoder_timestamp;
+    double dt = encoder->timestamp - last_encoder_timestamp;
+
+    dt /= (60.0 * 1000000.0); // microseconds / minute
+
+    double wheel_rpm[NUM_OF_WHEELS];
+    double setpoint[NUM_OF_WHEELS];
+    static double prev_cmd[NUM_OF_WHEELS];
+    double cmd[NUM_OF_WHEELS];
+
+
+    // Get wheel RPM
+    wheel_rpm[LEFT_WHEEL]  = encoderDataToRPM(encoder->left, dt);
+    wheel_rpm[RIGHT_WHEEL] = encoderDataToRPM(encoder->right, dt);
+
+    // Make local copy of setpoints
+    setpoint_mutex.lock();
+    setpoint[LEFT_WHEEL] = left_setpoint;
+    setpoint[RIGHT_WHEEL] = right_setpoint;
+    setpoint_mutex.unlock();
+
+
+    for(int i=0; i<NUM_OF_WHEELS; i++)
+    {
+        if(setpoint[i] > (double)DEADZONE || setpoint[i] < (double)-DEADZONE)
+        {
+            if(wheel_rpm[i] < setpoint[i])
+            {
+                double error = setpoint[i] - wheel_rpm[i];
+                cmd[i] = prev_cmd[i] + clamp(((error * error /10) + 1), 0, 50);
+            }
+            else
+            {
+                double error = wheel_rpm[i] - setpoint[i];
+                cmd[i] = prev_cmd[i] - clamp(((error * error /10) + 1), 0, 50);
+            }
+        }
+        else
+        {
+            cmd[i] = 0;
+        }
+        cmd[i] = clamp(cmd[i], -1000, 1000);
+    }
+
+#ifndef NDEBUG
+    cout << "Measured: L " << wheel_rpm[LEFT_WHEEL] << ", R " << wheel_rpm[RIGHT_WHEEL] << endl;
+    cout << "Setpoint: L " << setpoint[LEFT_WHEEL] << ", R " << setpoint[RIGHT_WHEEL] << endl;
+    cout << "Output:   L " << cmd[LEFT_WHEEL] << ", R " << cmd[RIGHT_WHEEL] << endl << endl;
+#endif //NDEBUG
+
+    motor_control_command->left = cmd[LEFT_WHEEL];
+    motor_control_command->right = cmd[RIGHT_WHEEL];
+    motor_control_command->putMessage();
+
+    for(int i=0 ; i<NUM_OF_WHEELS ; i++)
+    {
+        prev_cmd[i] = cmd[i];
+    }
+
+    last_encoder_timestamp = (uint64_t)encoder->timestamp;
 }
 
 /**
@@ -112,10 +202,10 @@ void auto_callback(void){
         right_motor_speed = 10;
     }
 
-
-    motor_control_command->left = left_motor_speed;
-    motor_control_command->right = right_motor_speed;
-    motor_control_command->putMessage();
+    setpoint_mutex.lock();
+    left_setpoint = left_motor_speed;
+    left_setpoint = right_motor_speed;
+    setpoint_mutex.unlock();
 }
 
 int main(int argc, char* argv[])
